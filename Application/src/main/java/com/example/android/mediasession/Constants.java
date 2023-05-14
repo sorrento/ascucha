864ded98be79c1fc7514d0d676b20519dd6bd0bf
MILENKO <369-90077310@users.noreply.gitlab.example.com>
2023-05-14 20:14:49 +0200

    cargo Chapter service antes de todo y hago que el resto se espere

--- Application/src/main/java/com/example/android/mediasession/Constants.java
+++ Application/src/main/java/com/example/android/mediasession/Constants.java
@@ -1,7 +1,8 @@
 package com.example.android.mediasession;
 
 public class Constants {
-
+    static final String APP_ID = "xx";
+    static final String CLIENT_KEY = "xx";
     public static boolean debugMode = false;
     public static final String PREFIX_OF_CHAPTER_UTTERANCE = "utu";
 }
